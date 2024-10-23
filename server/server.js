60580e8cd748d84ad721d634c50bc398c7ded721
Nesbeer <150609205+ItzNesbro@users.noreply.github.com>
2024-10-23 07:04:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -52,5 +52,9 @@ app.post("/api/summarize", async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
